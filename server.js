8353e81fabaf6869e076c96a15eff80169253061
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-26 18:53:56 -0700

    Server changes

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
+app.use(express.static(__dirname + '/public'));
+
 http.listen(PORT, function () {
   console.log("Server started!");
 });
