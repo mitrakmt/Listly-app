904247977e2d5d2320314034868c47bef05e9548
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-27 11:39:05 -0700

    Added functionality for 50 new websites

--- public/js/link-sidebar.js
+++ public/js/link-sidebar.js
@@ -1,6 +1,6 @@
 function addUrlIcon() {
     flag = false;
-    url = document.getElementById("urlSubmission").value;
+    url = document.getElementById("urlSubmission").value.toLowerCase();
     var $element = $('#fast-link');
     var numberOfChildren = $element.children().length;
 
@@ -82,6 +82,18 @@ var linkIcons = [
     icon: "fa-linkedin-square"
   },
   {
+    name: "gmail.com",
+    icon: "fa-envelope"
+  },
+  {
+    name: "esea.com",
+    icon: "fa-steam"
+  },
+  {
+    name: "steam.com",
+    icon: "fa-steam"
+  },
+  {
     name: "reddit.com",
     icon: "fa-reddit"
   },
@@ -136,6 +148,217 @@ var linkIcons = [
   {
     name: "soundcloud.com",
     icon: "fa-soundcloud"
+  },
+  {
+    name: "foursquare.com",
+    icon: "fa-foursquare"
+  },
+  {
+    name: "500px.com",
+    icon: "fa-500px"
+  },
+  {
+    name: "bitbucket.org",
+    icon: "fa-bitbucket"
+  },
+  {
+    name: "stripe.com",
+    icon: "fa-cc-stripe"
+  },
+  {
+    name: "gitlab.com",
+    icon: "fa-gitlab"
+  },
+  {
+    name: "getpocket.com",
+    icon: "fa-get-pocket"
+  },
+  {
+    name: "mixcloud.com",
+    icon: "fa-mixcloud"
+  },
+  {
+    name: "paypal.com",
+    icon: "fa-paypal"
+  },
+  {
+    name: "chase.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "bankofamerica.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "citigroup.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "wellsfargo.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "usbank.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "bnymellon.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "pnc.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "capitalone.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "hsbc.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "tdbank.com",
+    icon: "fa-credit-card"
+  },
+  {
+    name: "apple.com",
+    icon: "fa-apple "
+  },
+  {
+    name: "drupal.com",
+    icon: "fa-drupal"
+  },
+  {
+    name: "qq.com",
+    icon: "fa-qq"
+  },
+  {
+    name: "yahoo.com",
+    icon: "fa-yahoo"
+  },
+  {
+    name: "xing.com",
+    icon: "fa-xing"
+  },
+  {
+    name: "yelp.com",
+    icon: "fa-yelp"
+  },
+  {
+    name: "espn.com",
+    icon: "fa-futbol-o"
+  },
+  {
+    name: "premierleague.com",
+    icon: "fa-soccer-ball-o"
+  },
+  {
+    name: "nfl.com",
+    icon: ""
+  },
+  {
+    name: "nhl.com",
+    icon: "fa-circle"
+  },
+  {
+    name: "nba.com",
+    icon: "fa-futbol-o"
+  },
+  {
+    name: "nordstroms.com",
+    icon: "fa-shopping-bag"
+  },
+  {
+    name: "buzzfeed.com",
+    icon: "share-alt-square"
+  },
+  {
+    name: "classpass.com",
+    icon: "fa-blind"
+  },
+  {
+    name: "dribbble.com",
+    icon: "fa-dribbble"
+  },
+  {
+    name: "codepie.tv",
+    icon: "fa-codiepie"
+  },
+  {
+    name: "discover.com",
+    icon: "fa-cc-discover"
+  },
+  {
+    name: "codepen.io",
+    icon: "fa-codepen"
+  },
+  {
+    name: "digg.com",
+    icon: "fa-digg"
+  },
+  {
+    name: "css3.info",
+    icon: "fa-css3"
+  },
+  {
+    name: "jsfiddle.net",
+    icon: "fa-jsfiddle"
+  },
+  {
+    name: "scribd.com",
+    icon: "fa-scribd "
+  },
+  {
+    name: "slack.com",
+    icon: "fa-slack "
+  },
+  {
+    name: "tripadvisor.com",
+    icon: "fa-tripadvisor"
+  },
+  {
+    name: "vine.co",
+    icon: "fa-vine"
+  },
+  {
+    name: "stumbleupon.com",
+    icon: "fa-stumbleupon"
+  },
+  {
+    name: "stackexchange.com",
+    icon: "fa-stack-exchange"
+  },
+  {
+    name: "skype.com",
+    icon: "fa-skype"
+  },
+  {
+    name: "leanpub.com",
+    icon: "fa-leanpub"
+  },
+  {
+    name: "git-scm.com",
+    icon: "fa-git-square"
+  },
+  {
+    name: "houzz.com",
+    icon: "fa-houzz "
+  },
+  {
+    name: "opencart.com",
+    icon: "fa-opencart"
+  },
+  {
+    name: "vimeo.com",
+    icon: "fa-vimeo"
+  },
+  {
+    name: "renren.com",
+    icon: "fa-renren"
+  },
+  {
+    name: "medium.com",
+    icon: "fa-medium"
   }
-
 ]
--- public/js/news.js
+++ public/js/news.js
@@ -1,34 +1,44 @@
+var randomSectionSelector = function () {
+  return Math.ceil(Math.random() * 11)
+}
 
 var getNews = function () {
 
   return new Promise(function(resolve, reject) {
 
-    var newsUrl = 'https://webhose.io/search?token=6edaf299-4120-490a-a343-cd2bcf0bc611&format=json&q=performance_score%3A%3E8&ts=1472179211545';
+    var sections = ["home", "opinion", "world", "national", "politics", "business", "technology", "science", "health", "sports", "travel", "insider"]
+    var randomSection = sections[randomSectionSelector()];
 
-      $.getJSON(newsUrl, function (news) {
-        if (news) {
+    var url = 'https://api.nytimes.com/svc/topstories/v2/' + randomSection + '.json';
+    url += '?' + $.param({
+      'api-key': "ba8b979781bc481d9d4fdb7508cfeae4"
+    });
+    $.ajax({
+      url: url,
+      method: 'GET',
+    }).done(function(news) {
+
+      if (news) {
 
           var storyToDisplay = {};
 
-          var filteredStories = news.posts.filter(function(story) {
-            return story.language === 'english' && story.thread.country === 'US' && story.thread.performance_score >= 9 && story.thread.spam_score < 0.1 && story.thread.replies_count >= 5;
-          })
+          // var filteredStories = news.posts.filter(function(story) {
+          //   return story.language === 'english' && story.thread.country === 'US' && story.thread.performance_score >= 9 && story.thread.spam_score < 0.1 && story.thread.replies_count >= 5;
+          // })
 
-          var topStory = filteredStories[0];
-          storyToDisplay.author = topStory.author;
+          var topStory = news.results[0];
           storyToDisplay.url = topStory.url;
           storyToDisplay.title = topStory.title;
-          storyToDisplay.text = topStory.text;
-          storyToDisplay.image = topStory.thread.main_image;
-          storyToDisplay.replies = topStory.thread.replies_count;
+          storyToDisplay.abstract = topStory.abstract;
 
           $(".news-title").append('<a href="' + storyToDisplay.url + '" target="_blank">' + storyToDisplay.title + '</a>');
-          $(".news-text").append("<small>" + storyToDisplay.text + "</small>")
-          $(".news-extras").append('<small>Replies: ' + storyToDisplay.replies + '</small>')
-          resolve(news);
-        } else {
-          reject(Error("Failed to grab stock quite information"));
-        }
-      }, "jsonp");
+          $(".news-text").append("<small>" + storyToDisplay.abstract + "</small>")
+          $(".news-extras").append("<small>" + randomSection + "</small>")
+          resolve(topStory);
+
+    } else {
+      reject(Error("Failed to grab news article"));
+    }
+    });
   });
 };
