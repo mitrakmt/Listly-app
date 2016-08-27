0354c4777a89385ed51d354dd20fdd23dec82fdf
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-26 18:49:30 -0700

    Updated package.js

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var PORT = process.env.PORT || 3000;
 var express = require('express');
+var app = express();
+var http = require('http').Server(app);
 
 http.listen(PORT, function () {
   console.log("Server started!");
