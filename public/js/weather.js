6bb5e0babc318f49e80a5337631df3aa60e3279d
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-08-31 15:05:33 -0700

    Fixed weather interface

--- public/js/weather.js
+++ public/js/weather.js
@@ -5,21 +5,26 @@ var findWeather = function(location) {
             return reject("There was an error in grabbing location");
         }
 
-        var weatherUrl = 'http://api.wunderground.com/api/0a4f6196a6ca6510/conditions/q/' + location.region + '/' + location.city + '.json';
+        var city = location.city.replace(" ", "_");
 
-        var currentTime = moment().format("h");
+        var weatherUrl = 'http://api.wunderground.com/api/0a4f6196a6ca6510/conditions/q/' + location.region + '/' + city + '.json';
+
+        var currentTime = moment().format("H");
+        console.log(currentTime);
 
         $.getJSON(weatherUrl, function(weatherInfo) {
+          console.log(weatherInfo)
             var weatherIcon;
-            if (weatherInfo.currently.icon.indexOf("rain") !== -1) {
+            var current = weatherInfo.current_observation;
+            if (current.icon.indexOf("rain") !== -1) {
                 weatherIcon = "fa-umbrella"
-            } else if (weatherInfo.currently.icon.indexOf("storm") !== -1) {
+            } else if (current.icon.indexOf("storm") !== -1) {
                 weatherIcon = "fa-flash"
-            } else if (weatherInfo.currently.icon.indexOf("cloud") !== -1) {
+            } else if (current.icon.indexOf("cloud") !== -1) {
                 weatherIcon = "fa-cloud"
-            } else if (weatherInfo.currently.windspeed > 20) {
+            } else if (current.windspeed > 20) {
                 weatherIcon = "fa-envira"
-            } else if (weatherInfo.currently.temperature < 60) {
+            } else if (current.temperature < 60) {
                 weatherIcon = "fa-frost-o"
             } else if (currentTime < 5 || currentTime >= 21) {
                 weatherIcon = "fa-moon-o"
@@ -27,7 +32,7 @@ var findWeather = function(location) {
                 weatherIcon = "fa-sun-o"
             }
 
-            $(".weather").append('<div class="col-md-5 weather-info weather-city"><h5>' + location.city + '</h5></div><div class="col-md-5 weather-info"><h4>' + weatherInfo.currently.temperature + '°</h4><i class="fa ' + weatherIcon + ' fa-3x"></i><h6 class="weather-summary"><small>' + weatherInfo.currently.summary + '</small></h6></div>');
+            $(".weather").append('<div class="col-md-5 weather-info weather-city"><h5>' + location.city + '</h5></div><div class="col-md-5 weather-info"><h4>' + current.feelslike_f + '°</h4><i class="fa ' + weatherIcon + ' fa-3x"></i><h6 class="weather-summary"><small>' + current.weather + '</small></h6></div>');
         });
     });
 }
