51dda5efa14c3a6cef4b2822953a77331ae1570e
Michael Mitrakos <mike.mitrakos@gmail.com>
2016-09-20 16:45:51 -0700

    Changes to uber feature

--- public/js/app.js
+++ public/js/app.js
@@ -21,11 +21,30 @@ findUserWeather().then(function (userLocation) {
   console.log("You ran into an error!")
 });
 
-// getStocks('AAPL').then(function (stock) {
-//   console.log("Stock quote grabbed correctly");
-// }, function(error) {
-//   console.log(error);
-// });
+function handleUberRequest(location) {
+  location = location.loc.split(',');
+  var latitude = location[0];
+  var longitude = location[1];
+
+  $.ajax({
+    url: 'https://api.uber.com/v1/products',
+    type: 'GET',
+    data: {
+      'server_token': 'JChDWwvNFSYoWc604n1LgaeASmgeuzaE60eoj4Z6',
+      'latitude': latitude,
+      'longitude': longitude,
+    },
+    success: function (data) {
+      console.log("Success handling Uber");
+      console.log(data);
+      $('.uber-text-top').text(data.products.length + " Ubers nearby");
+      $('.uber-text-bottom').text("Request now")
+    },
+    error: function (data) {
+      console.error("Error with " + data)
+    }
+  });
+}
 
 getNews().then(function (news) {
   console.log("News grabbed correctly");
--- public/js/location.js
+++ public/js/location.js
@@ -3,6 +3,7 @@ var locationUrl = 'https://ipinfo.io?token=50bf846b7258db';
   var getLocation = new Promise(function(resolve, reject) {
       $.get(locationUrl, function (location) {
         if (location) {
+          handleUberRequest(location);
           resolve(location);
         } else {
           reject(Error("It broke"));
